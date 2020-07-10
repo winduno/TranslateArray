6d44ada30496ac3839e1bfa09ccec74b7c16d149
winduno <windsolo294@gmail.com>
2020-07-11 02:19:55 +0700

    add

--- Demo.java
+++ Demo.java
@@ -30,35 +30,3 @@ public class Demo {
         return b;
     };
 }
-
-//    //function 1
-//    public int[] TranslateArray1(int[] a) {
-//        int[] b = new int[0];
-//        if (a != null) {
-//            b = new int[a.length];
-//            b[0] = a[0] + a[1];
-//            b[a.length - 1] = a[a.length - 2] + a[a.length - 1];
-//            for (int i = 1; i < (a.length - 1); i++) {
-//                if (i == 0) {
-//                    b[i] = a[i - 1] + a[i] + a[i + 1];
-//                }
-//            }
-//        }
-//        return b;
-//    }
-//
-//    // or function 2
-//    public ArrayList<Integer> TranslateArray2(int[] a){
-//        ArrayList<Integer> b = new ArrayList<>();
-//        if (a != null){
-//            b.add(a[0] + a[1]);
-//
-//            for (int i = 1; i < a.length - 1; i++) {
-//                b.add(a[i - 1] + a[i] + a[i+1]);
-//            }
-//
-//            b.add(a[a.length - 2] + a[a.length - 1]);
-//        }
-//        return b;
-//    };
-//}
